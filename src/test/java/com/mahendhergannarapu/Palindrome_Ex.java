7c1718228b372b29b7a052dd426cee6b0f72d8fa
Mahendher-Gannarapu <gannarapu.mahendher99@gmail.com>
2025-07-20 17:22:40 +0530

    Add Files

--- src/test/java/com/mahendhergannarapu/Palindrome_Ex.java
+++ src/test/java/com/mahendhergannarapu/Palindrome_Ex.java
@@ -1,4 +1,21 @@
 package com.mahendhergannarapu;
 
 public class Palindrome_Ex {
+    public static void main(String[] args) {
+
+        String str1 = "Madam";
+        String str= str1.toLowerCase();
+        StringBuilder result = new StringBuilder(str).reverse();
+        System.out.println(result);
+
+        if(str.equals(result.toString()))
+        {
+            System.out.println("String is polindrome");
+        }
+        else
+        {
+            System.out.println("String not Polindrome");
+        }
+    }
+
 }
--- src/test/java/com/mahendhergannarapu/Reverse.java
+++ src/test/java/com/mahendhergannarapu/Reverse.java
@@ -1,4 +1,12 @@
+//String Reverse
 package com.mahendhergannarapu;
 
 public class Reverse {
+    public static void main(String[] args) {
+
+        String str = "madam";
+        StringBuilder result = new StringBuilder(str).reverse();
+        System.out.println(result);
+
+    }
 }
